9a5ca161b30ecd3782c48d76c232713654031155
Subham7008 <subhamsankar9.com>
2025-05-17 11:04:53 +0530

    fixed 404 not found issue

--- Quick_Serve/middlewares/cors.middleware.js
+++ Quick_Serve/middlewares/cors.middleware.js
@@ -2,7 +2,7 @@ const cors = require('cors');
 
 const corsMiddleware = cors({
   origin: process.env.NODE_ENV === 'production' 
-    ? [process.env.FRONTEND_URL, 'https://quick-serve-production.up.railway.app', 'https://quick-serve-anubce8cv-subham-shankar-sahoos-projects.vercel.app', 'https://quick-serve-lt8ap4a3z-subham-shankar-sahoos-projects.vercel.app', 'https://quick-serve-six.vercel.app', 'https://quick-serve-803tm4hn2-subham-shankar-sahoos-projects.vercel.app'] 
+    ? [process.env.FRONTEND_URL, 'https://quick-serve-production.up.railway.app', 'https://quick-serve-anubce8cv-subham-shankar-sahoos-projects.vercel.app', 'https://quick-serve-lt8ap4a3z-subham-shankar-sahoos-projects.vercel.app', 'https://quick-serve-six.vercel.app', 'https://quick-serve-803tm4hn2-subham-shankar-sahoos-projects.vercel.app', 'https://quick-serve-2cjk8ydho-subham-shankar-sahoos-projects.vercel.app'] 
     : ['http://localhost:5173', 'http://127.0.0.1:5173', 'http://[::1]:5173', 'http://localhost:3333', 'http://127.0.0.1:3333', 'http://[::1]:3333'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'Origin', 'X-Requested-With'],
